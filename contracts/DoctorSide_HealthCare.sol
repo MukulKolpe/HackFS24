d7af90d2634718680e3d6815d8398ba3ee01bc83
SarveshLimaye <sarveshlimaye2002@gmail.com>
2024-06-02 18:49:11 +0530

    healtcare contract updated

--- contracts/DoctorSide_HealthCare.sol
+++ contracts/DoctorSide_HealthCare.sol
@@ -1,107 +1,129 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+import "./UserSide_Healthcare.sol";
 
-contract UserSide_Healthcare{
-
-    //roles:
-    // 1: System Admin
-    // 2: Doctor
-    // 3: Patient
-    // 4: Other Hospital Employees
-    // 5: Lab Employees
-
-    uint256 public totalUsers = 1;
-    struct User{
-        uint256 userId;
-        string userName;
-        string userAadhar;
-        string userLicenseNo;
-        uint256 userAge;
-        string userEmail;
-        string userSpeciality;
-        string userProfileImg;
-        string userMedicalDegree;
-        uint256 userRole;
-        address userWalletAddress;
-        bool isVerified;
+contract DoctorSide_Healthcare is UserSide_Healthcare{
+    uint256 public totalAppointments = 1;
+    uint256 public totalSlots = 1;
+    uint256 public totalDocuments = 1;
+    struct Appointment {
+        uint256 appId;
+        string appDate;
+        string startTime;
+        string endTime;
+        string appSubject;
+        string appFeedback;
+        address patientWalletAddress;
+        address doctorWalletAddress;
+        bool isApproved;
+        uint256 slotId;
+        uint256 appType;
     }
 
-    struct PatientHistory{
-        uint256 userId;
-        bool isHandicap;
-        bool isBp;
-        bool isDiabetes;
-        uint256 userExp;
+    struct AppointmentTimeSlot{
+        uint256 slotId;
+        string slotDate;
+        string startTime;
+        string endTime;
+        bool isBooked;
     }
 
-    mapping(address => uint256) public userWalletAddresstoUserId;
-    mapping(uint256 => User) public userIdtoUser;
-    mapping(uint256 => bool) public userIdtoBlacklist;
-    mapping(uint256 => uint256) public userIdtoReportUser;
-    mapping(uint256 => PatientHistory) public userIdtoPatientHistory;
-    mapping(uint256 => string[]) public userIdtoPrevMedicalHistory; // mapping 6
-    mapping(string => uint256) public userEmailtoUserId;
-    address public admin1;
-    address public admin2 = 0x4F53d589FE623aF6D0dc3ccA612C2F35F64d3cb9;
-    address public admin3 = 0xc35fC43ae078961BfC34FfB6c2148571b6f87920;
-
-
-    function createUser(string memory _userName,string memory _userAadhar,string memory _userLicenseNo,uint256 _userAge,string memory _userEmail,string memory _userSpeciality,string memory _userProfileImg,string memory _userMedicalDegree,uint256 _userRole) public {
-        if(_userRole == 3){
-            User memory u1 = User(totalUsers,_userName,_userAadhar,_userLicenseNo,_userAge,_userEmail,_userSpeciality,_userProfileImg,_userMedicalDegree,_userRole,msg.sender,false);
-            userIdtoUser[totalUsers] = u1;
-            takeUserHistory(totalUsers, false, false, false, 0);
-            require(userWalletAddresstoUserId[u1.userWalletAddress] == 0 && userEmailtoUserId[u1.userEmail] == 0,"User with this wallet address or email already exists");
-            userIdtoUser[totalUsers].isVerified = true;
-            userWalletAddresstoUserId[u1.userWalletAddress] = u1.userId;
-            userEmailtoUserId[u1.userEmail] = u1.userId;
-            userIdtoBlacklist[totalUsers] = false;
-            userIdtoReportUser[totalUsers] = 0;
-            totalUsers++;
-        }
-        else{
-            User memory u1 = User(totalUsers,_userName,_userAadhar,_userLicenseNo,_userAge,_userEmail,_userSpeciality,_userProfileImg,_userMedicalDegree,_userRole,msg.sender,false);
-            userIdtoUser[totalUsers] = u1;
-            takeUserHistory(totalUsers, false, false, false, 0);
-            totalUsers++;
-        }
-        
+    struct PatientReport{
+        uint256 reportId;
+        string reportName;
+        address patientWalletAddress;
+        address doctorWalletAddress;
+        string ipfsHash;
     }
 
-    function takeUserHistory (uint256 _userId,bool _isHandicap,bool _isBp,bool _isDiabetes,uint256 _userExp) public{
-        PatientHistory memory pt1 = PatientHistory(totalUsers,_isHandicap,_isBp,_isDiabetes,_userExp);
-        userIdtoPatientHistory[_userId] = pt1;
+    mapping(uint256 => Appointment) public appointmentIdtoAppointment;
+    mapping(uint256 => uint256[]) public docIdtoAppointmentId; //mapping1
+    mapping(uint256 => uint256[]) public patIdtoAppointmentId; //mapping2
+    mapping(uint256 => mapping (string => AppointmentTimeSlot[])) public doctorIdtoDatetoTimeSlot; //mapping3
+    mapping(uint256 => bool) public slotIdtoBookingStatus;
+    mapping(uint256 => AppointmentTimeSlot) public appointmentSlotIdtoAppointmentSlot;
+    mapping(uint256 => PatientReport) public ReportIdtoPatintReport;
+    mapping(uint256 => PatientReport[]) public patientIdtoReport; // mapping 4
+    mapping(uint256 => PatientReport[]) public doctortIdtoReport; // mapping 5
+
+    function uploadMedicalReprt(string memory _reportName,address _patientWalletAdress,address _doctorWalletAddress,string memory _ipfsHash) public {
+        PatientReport memory r1 = PatientReport(totalDocuments,_reportName,_patientWalletAdress,_doctorWalletAddress,_ipfsHash);
+        ReportIdtoPatintReport[totalDocuments] = r1;
+        uint256 patId  = userWalletAddresstoUserId[_patientWalletAdress];
+        uint256 doctorId = userWalletAddresstoUserId[_doctorWalletAddress];
+        patientIdtoReport[patId].push(r1);
+        doctortIdtoReport[doctorId].push(r1);
+        totalDocuments++;
+    }
+
+    function openTimeslots(string memory _slotDate,string memory _startTime,string memory _endTime)public{
+        uint256 tempUserId = userWalletAddresstoUserId[msg.sender];
+        User memory u1 = userIdtoUser[tempUserId];
+        require(u1.userRole == 2 && u1.isVerified,"Only verified doctor can call this function");
+        AppointmentTimeSlot memory a1 = AppointmentTimeSlot(totalSlots,_slotDate,_startTime,_endTime,false);
+        doctorIdtoDatetoTimeSlot[tempUserId][_slotDate].push(a1);
+        appointmentSlotIdtoAppointmentSlot[totalSlots] = a1;
+        totalSlots++;
+    }
+
+    function bookAppointment(string memory _appDate,string memory _startTime,string memory _endTime,string memory _appSubject,string memory _appFeedback,address _doctorWalletAddress,uint256 _slotId) public {
+        Appointment memory a1 = Appointment(totalAppointments,_appDate,_startTime,_endTime,_appSubject,_appFeedback,msg.sender,_doctorWalletAddress,false,_slotId,0);
+        appointmentIdtoAppointment[totalAppointments] = a1;
+        uint256 patId = userWalletAddresstoUserId[msg.sender];
+        uint256 doctorId = userWalletAddresstoUserId[_doctorWalletAddress];
+        require(patId != 0 && doctorId != 0,"Patient wallet address and wallet address must be both registered into the system");
+        require(!userIdtoBlacklist[patId] && !userIdtoBlacklist[doctorId],"Either patient or doctor are blacklisted");
+        docIdtoAppointmentId[doctorId].push(totalAppointments);
+        patIdtoAppointmentId[patId].push(totalAppointments);
+        totalAppointments++;
+    }
+
+    function bookAppointmentEmergency(string memory _appDate,string memory _startTime,string memory _endTime,string memory _appSubject,string memory _appFeedback,address _doctorWalletAddress,uint256 _slotId) public payable{
+        uint256 stakeAmount = 1 * 10**14;
+        require(msg.value >= stakeAmount,"You need to stake more amount in order to book emergency appointment");
+        Appointment memory a1 = Appointment(totalAppointments,_appDate,_startTime,_endTime,_appSubject,_appFeedback,msg.sender,_doctorWalletAddress,false,_slotId,1);
+        appointmentIdtoAppointment[totalAppointments] = a1;
+        uint256 patId = userWalletAddresstoUserId[msg.sender];
+        uint256 doctorId = userWalletAddresstoUserId[_doctorWalletAddress];
+        require(patId != 0 && doctorId != 0,"Patient wallet address and wallet address must be both registered into the system");
+        require(!userIdtoBlacklist[patId] && !userIdtoBlacklist[doctorId],"Either patient or doctor are blacklisted");
+        docIdtoAppointmentId[doctorId].push(totalAppointments);
+        patIdtoAppointmentId[patId].push(totalAppointments);
+        totalAppointments++;
+    }
+
+
+    function approveAppointment(uint256 _appId) public {
+        uint256 doctorId = userWalletAddresstoUserId[appointmentIdtoAppointment[_appId].doctorWalletAddress];
+        require(msg.sender == userIdtoUser[doctorId].userWalletAddress,"Invalid doctor to approve the appointment");
+        require(userIdtoUser[doctorId].userRole == 2,"Only doctors can call this function");
+
+        appointmentIdtoAppointment[_appId].isApproved = true;
+        slotIdtoBookingStatus[appointmentIdtoAppointment[_appId].slotId] = true;
     }
 
     constructor(){
-        admin1 = msg.sender;
+        createUser("System Admin", "9022 2000 1022 0911", "109869", 0,"kayalsoham61@gmail.com", "Moderator", "ipfs.io/ipfs/", "ipfs.io/ipfs/", 1);
+        approveUser(1);
     }
 
-    function approveUser(uint256 _userId) public {
-        require(msg.sender == admin1 || msg.sender == admin2 || msg.sender == admin3,"Only Admin can call this function");
-        User memory u1 = userIdtoUser[_userId];
-        require(userWalletAddresstoUserId[u1.userWalletAddress] == 0 && userEmailtoUserId[u1.userEmail] == 0,"User with this wallet address or email already exists");
-        userIdtoUser[_userId].isVerified = true;
-        userWalletAddresstoUserId[u1.userWalletAddress] = u1.userId;
-        userEmailtoUserId[u1.userEmail] = u1.userId;
-        userIdtoBlacklist[_userId] = false;
-        userIdtoReportUser[_userId] = 0;
+    function getMapping1length(uint256 _doctorId) public view returns (uint256) {
+        return docIdtoAppointmentId[_doctorId].length;
     }
 
-    function disApproveUser(uint256 _userId) public {
-        require(msg.sender == admin1 || msg.sender == admin2 || msg.sender == admin3,"Only admin can call this function");
-        userIdtoUser[_userId].isVerified = false;
-        userIdtoBlacklist[_userId] = true;
+    function getMapping2length(uint256 _patientId) public view returns (uint256) {
+        return patIdtoAppointmentId[_patientId].length;
     }
 
-    function reportUser(uint256 _userId) public {
-        userIdtoReportUser[_userId]++;
-        if(userIdtoReportUser[_userId] > 100){
-            userIdtoBlacklist[_userId] = true;
-        }
+    function getMapping3length(uint256 _doctorId,string memory _date) public view returns (uint256) {
+        return doctorIdtoDatetoTimeSlot[_doctorId][_date].length;
     }
 
-    function getMapping6length(uint256 _userId) public view returns(uint256) {
-        return userIdtoPrevMedicalHistory[_userId].length;
+    function getMapping4length(uint256 _patientId) public view returns (uint256) {
+        return patientIdtoReport[_patientId].length;
+    }
+
+    function getMapping5length(uint256 _doctorId) public view returns (uint256) {
+        return doctortIdtoReport[_doctorId].length;
     }
 }
